5446a768f3fcbca9833e2da11afc366b010eb5a7
Joon Sunn Foo <joonsunn.foo@gmail.com>
2026-01-17 20:31:26 +0800

    feat: save theme to async storage

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,5 +1,6 @@
 import { darkTheme, lightTheme } from "@/constants/my-theme";
 import { ThemeContextType } from "@/types/theme";
+import { getSavedTheme, setSavedTheme } from "@/utils/theme";
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
 import { useColorScheme } from "react-native";
 
@@ -10,14 +11,28 @@ export const ThemeProvider = ({ children }: PropsWithChildren) => {
   const [theme, setTheme] = useState(lightTheme);
 
   useEffect(() => {
+    // Load saved theme from AsyncStorage
+    const loadSavedTheme = async () => {
+      const savedTheme = await getSavedTheme();
+      console.log({ savedTheme });
+      if (savedTheme) {
+        setTheme(savedTheme === "dark" ? darkTheme : lightTheme);
+      }
+    };
+    loadSavedTheme();
+  }, []);
+
+  useEffect(() => {
     // Auto-switch based on system preference
     const newTheme = colorScheme === "dark" ? darkTheme : lightTheme;
     setTheme(newTheme);
   }, [colorScheme]);
 
   // Manual toggle function (optional)
-  const toggleTheme = () => {
+  const toggleTheme = async () => {
     setTheme((prev) => (prev === lightTheme ? darkTheme : lightTheme));
+    const savedTheme = await getSavedTheme();
+    setSavedTheme(savedTheme === "dark" ? "light" : "dark");
   };
 
   return <ThemeContext value={{ theme, toggleTheme, isDark: theme === darkTheme }}>{children}</ThemeContext>;
